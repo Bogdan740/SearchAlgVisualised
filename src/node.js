3a48269858a2617a0f5da7169ad4fdb1975e669a
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2023-01-21 19:35:30 +0000

    re-organise dir structure

--- src/node.js
+++ src/node.js
@@ -1,43 +1,43 @@
-let offset = 0.5;
+let offset = 0;
 
 class Node {
-  constructor(posx, posy, squareSize, target, type, visited) {
-    this.pos = createVector(posx * squareSize, posy * squareSize);
-    this.realPos = createVector(this.pos.x + squareSize / 2, this.pos.y + squareSize / 2);
+  constructor(posx, posy, squareSize, target, type) {
+    this.pos = createVector(posx, posy);
+    this.realPos = createVector(
+      this.pos.x * squareSize + squareSize / 2,
+      this.pos.y * squareSize + squareSize / 2
+    );
     this.type = type;
     this.squareSize = squareSize;
-    // Currently using manhattan distance
+    // Manhattan distance
     this.proxToTarget = round(
       dist(posx, posy, posx, target.y) + dist(posx, posy, target.x, posy),
       2
     );
-    if (type === nodeTypes.start) {
-      this.col = 'green';
-    } else if (type === nodeTypes.end) {
-      this.col = 'red';
-    } else if (type === nodeTypes.empty) {
-      this.col = '#ffffff';
-    } else {
-      console.warning(`Invalid initializing node type : ${type}`);
-    }
-    this.visited = visited;
+    this.previous = null;
   }
 
   show() {
     push();
-    fill(this.col);
-    rect(this.pos.x, this.pos.y, this.squareSize - offset, this.squareSize - offset);
-    stroke(0);
+    let col = 'white';
+    // TODO : Use a case statement
+    if (this.type == nodeTypes.start) {
+      col = 'green';
+    } else if (this.type === nodeTypes.end) {
+      col = 'red';
+    } else if (this.type === nodeTypes.visited) {
+      col = 'blue';
+    } else if (this.type === nodeTypes.obstacle) {
+      col = 'black';
+    }
+    fill(col);
+    rect(this.realPos.x, this.realPos.y, this.squareSize - offset, this.squareSize - offset);
     pop();
   }
 
-  setVisited() {
-    self.visited = true;
-  }
-  setPath() {
-    self.type = nodeTypes.path;
-  }
-  setObstacle() {
-    self.types = nodeTypes.obstacle;
+  setType(type) {
+    if (this.type != 'end' && this.type != 'start') {
+      this.type = type;
+    }
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,20 +1,22 @@
 const nodeTypes = {
   obstacle: 'obstacle',
   empty: 'empty',
+  visited: 'visited',
   path: 'path',
   start: 'start',
   end: 'end',
 };
 
-let gridSize = 10; // Number of squares in one row/col of the N x N grid
+let gridSize = 5; // Number of squares in one row/col of the N x N grid
 let squareSize; // Side length of individual square in the grid
 let nodes = [];
 let node;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  squareSize = Math.floor(windowWidth / gridSize) - 50;
+  squareSize = Math.floor(700 / gridSize);
 
+  // Initialize the grid of nodes
   for (let i = 0; i < gridSize; i++) {
     let strip = [];
     for (let j = 0; j < gridSize; j++) {
@@ -25,19 +27,62 @@ function setup() {
         type = nodeTypes.end;
       }
 
-      strip.push(new Node(i, j, squareSize, createVector(1, 1), type, false));
+      strip.push(new Node(i, j, squareSize, createVector(1, 1), type));
     }
     nodes.push(strip);
   }
-
-  console.log(nodes);
 }
 
+const nbours = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+let queue = [[0, 0, null]];
+let path = [];
 function draw() {
-  background(255, 255, 255);
+  // BFS
+  for (let i = 0; i < queue.length; i++) {
+    let toCheck = queue.shift();
+    let nodeToCheck = nodes[toCheck[0]][toCheck[1]];
+    if (nodeToCheck.type === nodeTypes.visited) continue;
+    nodeToCheck.setType(nodeTypes.visited);
+    nodeToCheck.previous = toCheck[2];
+    nbours.forEach((nbour) => {
+      let nx = nodeToCheck.pos.x + nbour[0];
+      let ny = nodeToCheck.pos.y + nbour[1];
+
+      if (0 <= nx && nx < gridSize && 0 <= ny && ny < gridSize) {
+        if (nodes[nx][ny].type !== nodeTypes.visited) {
+          queue.push([nx, ny, nodeToCheck]);
+        }
+      }
+    });
+  }
+
+  if (queue.length == 0 && path.length == 0) {
+    // Find the path starting from the end node and working backwards, store it in path
+    let currentNode = nodes[gridSize - 1][gridSize - 1];
+    while (currentNode) {
+      path.push(currentNode.pos);
+      currentNode = currentNode.previous;
+    }
+  }
+
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       nodes[i][j].show();
+    }
+  }
+
+  if (path.length != 0) {
+    for (let i = 0; i < path.length; i++) {
+      push();
+      fill('yellow');
+      ellipse(path[i].x * squareSize, path[i].y * squareSize, 10);
+      pop();
     }
   }
 }
